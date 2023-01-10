11b570724feaf1f28bcc6d89bc28dc4783bd4ced
Christina Woodhams <cwoodhams89@gmail.com>
2023-01-10 18:13:23 -0500

    Third commit. Card styling for animals and like functionality complete.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import AnimalShow from './AnimalShow';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function getRrandomAnimal (){
+    const animals= ['bird', 'cat', 'cow', 'dog', 'gator', 'horse']
+    return animals[Math.floor(Math.random() * animals.length)];
+};
+console.log(getRrandomAnimal())
+
+function App () {
+    const [animals, setAnimals] = useState([]);
+
+    const handleClick = () => {
+        setAnimals([...animals, getRrandomAnimal()]) //Creates a new array with our existing animals in addition to one more
+    };
+
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow 
+            type={animal} 
+            key={index} />
+    })
+
+    return (
+    <div className='app'>
+        <button  onClick={handleClick}>Add Animal</button>
+        <div className='animal-list'>{renderedAnimals}</div>
     </div>
-  );
+)
 }
 
 export default App;
